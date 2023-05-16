7d6057f1cbf88154587c8199d719264d42dd4e36
Rohit M Patil <roniepatil@gmail.com>
2023-05-16 19:50:42 -0400

    First commit

---
+++ Arrays & Hashing/Group_Anagrams.py
@@ -0,0 +1,29 @@
+"""
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Example 1:
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Example 2:
+Input: strs = [""]
+Output: [[""]]
+
+Example 3:
+Input: strs = ["a"]
+Output: [["a"]]
+
+"""
+
+from typing import List
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap = {}
+        for s in strs:
+            key = ''.join(sorted(s))
+            if key in hashmap:
+                hashmap[key].append(s)
+            else:
+                hashmap[key] = [s]
+        return list(hashmap.values())
---
+++ README.md
@@ -0,0 +1,11 @@
+## Description
+This repository includes all the solutions for LeetCode problems.
+
+## Author
+Rohit M Patil
+
+## Roadmap
+![Work space](/Images/RoadMap.png?raw=true)
+
+
+
