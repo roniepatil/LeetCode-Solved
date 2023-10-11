d542ff28d236c844319e10ee08e681553b2dbc53
Rohit M Patil <roniepatil@gmail.com>
2023-10-11 17:38:33 -0400

    Number_Of_Islands

---
+++ Graphs/Number_Of_Islands.py
@@ -0,0 +1,53 @@
+'''
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+
+Example 2:
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+'''
+from typing import List
+import collections
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+        islands = 0
+
+        def bfs(r, c):
+            q = collections.deque()
+            visited.add((r, c))
+            q.append((r, c))
+            while q:
+                r, c = q.popleft()
+                directions = [[-1,0],[1,0],[0,-1],[0,1]]
+                for dr, dc in directions:
+                    row, col = r+dr, c+dc
+                    if row in range(rows) and col in range(cols) and grid[row][col] == "1" and (row, col) not in visited:
+                        visited.add((row, col))
+                        q.append((row, col))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    bfs(r, c)
+                    islands += 1
+        
+        return islands
