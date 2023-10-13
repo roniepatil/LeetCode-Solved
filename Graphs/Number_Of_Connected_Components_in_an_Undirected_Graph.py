42cc22a2257629d3c0e94982671a474915e8a51b
Rohit M Patil <roniepatil@gmail.com>
2023-10-13 18:40:23 -0400

    Number_Of_Connected_Components_in_an_UndirectedG

---
+++ Graphs/Number_Of_Connected_Components_in_an_Undirected_Graph.py
@@ -0,0 +1,46 @@
+'''
+You have a graph of n nodes. You are given an integer n and an array edges where edges[i] = [ai, bi] indicates that there is an edge between ai and bi in the graph.
+Return the number of connected components in the graph.
+
+Example 1:
+Input: n = 5, edges = [[0,1],[1,2],[3,4]]
+Output: 2
+
+Example 2:
+Input: n = 5, edges = [[0,1],[1,2],[2,3],[3,4]]
+Output: 1
+'''
+from typing import List
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        par = [i for i in range(n)]
+        rank = [1]*n
+
+        def find(num):
+            res = num
+            while res != par[res]:
+                par[res] = par[par[res]]
+                res = par[par[res]]
+            return res
+
+
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+
+            if p1 == p2:
+                return 0
+            
+            if rank[p2] > rank[p1]:
+                par[p1] = p2
+                rank[p2] += rank[p1]
+            else:
+                par[p2] = p1
+                rank[p1] += rank[p2]
+
+            return 1
+
+        res = n
+        for n1, n2 in edges:
+            res -= union(n1, n2)
+
+        return res
