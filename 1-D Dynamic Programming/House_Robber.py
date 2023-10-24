8aed43a9b7805f9b0413ae810067d98d680a2ab2
Rohit M Patil <roniepatil@gmail.com>
2023-10-24 15:32:40 -0400

    House_Robber_solved

--- 1-D Dynamic Programming/House_Robber.py
+++ 1-D Dynamic Programming/House_Robber.py
@@ -17,7 +17,10 @@ Total amount you can rob = 2 + 9 + 1 = 12.
 from typing import List
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        print("hello")
-        pass
-        pass
-        pass
+        rob1, rob2 = 0, 0
+        # rob1, rob2, n, n+1, ....
+        for n in nums:
+            temp = max(n+rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
