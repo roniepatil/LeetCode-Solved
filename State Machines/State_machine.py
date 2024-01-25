e43b95c9bd38a5619415ecddec00a128802ac459
Rohit M Patil <roniepatil@gmail.com>
2024-01-24 22:04:41 -0500

    State machine edit

--- State Machines/State_machine.py
+++ State Machines/State_machine.py
@@ -21,23 +21,56 @@ class EmptyCartState(CheckoutState):
         print("Item added to cart.")
         return ItemAddedState()
     
+    def review_cart(self):
+        print("Cannot review an empty cart.")
+
+    def enter_shipping_info(self, info):
+        print("Cannot enter shipping info with an empty cart.")
+
+    def process_payment(self):
+        print("Cannot process payment with an empty cart.")
+    
 
 class ItemAddedState(CheckoutState):
     def add_item(self, item):
-        print("Item added to cart.")
+        print("Item added to the cart.")
 
     def review_cart(self):
-        print("Reviewing cart's content")
+        print("Reviewing cart contents.")
         return CartReviewedState()
+
+    def enter_shipping_info(self, info):
+        print("Cannot enter shipping info without reviewing the cart.")
+
+    def process_payment(self):
+        print("Cannot process payment without entering shipping info.")
     
 class CartReviewedState(CheckoutState):
+    def add_item(self, item):
+        print("Cannot add items after reviewing the cart.")
+
+    def review_cart(self):
+        print("Cart already reviewed.")
+
     def enter_shipping_info(self, info):
-        print("Shipping info added.")
-        return ShippingInfoEnteredState()
+        print("Entering shipping information.")
+        return ShippingInfoEnteredState(info)
+
+    def process_payment(self):
+        print("Cannot process payment without entering shipping info.")
     
 class ShippingInfoEnteredState(CheckoutState):
+    def add_item(self, item):
+        print("Cannot add items after entering shipping info.")
+
+    def review_cart(self):
+        print("Cannot review cart after entering shipping info.")
+
+    def enter_shipping_info(self, info):
+        print("Shipping information already entered.")
+
     def process_payment(self):
-        print("Payment processed.")
+        print("Processing payment with the entered shipping info.")
 
 
 # Create Context class
